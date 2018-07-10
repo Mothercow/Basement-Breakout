593110c3dff8440492366e0a033c1b0b28df7550
Ng Kai Zhuang <ngkz1234@gmail.com>
2018-07-10 16:09:55 +0800

    Fixed saw animation

--- Assets/Scripts/SawCollision.cs
+++ Assets/Scripts/SawCollision.cs
@@ -7,19 +7,27 @@ public class SawCollision : MonoBehaviour
     public GameObject inspectCloseButton;
     public GameObject gasParticles;
     public GameObject hacksawAnim;
+    public GameObject hacksawAnim2;
 
     public static bool hasAnimationEnded = false;
     public static bool startTimer = false;
     public static bool gasStart = false;
 
     private float timerAnimation = 3.0f;
+    private float timerAnimation2 = 3.0f;
+
+    bool startTimer2 = false;
+    bool hasAnimationEnded2 = false;
 
     Animator sawAnimator;
+    Animator sawAnimator2;
 
     private void Start()
     {
         sawAnimator = hacksawAnim.GetComponent<Animator>();
+        sawAnimator2 = hacksawAnim2.GetComponent<Animator>();
         hacksawAnim.SetActive(false);
+        hacksawAnim2.SetActive(false);
     }
 
     private void Update()
@@ -31,18 +39,39 @@ public class SawCollision : MonoBehaviour
                 timerAnimation -= Time.deltaTime;
             }
         }
+
+        if(startTimer2 == true)
+        {
+            if (timerAnimation2 > 0)
+            {
+                timerAnimation2 -= Time.deltaTime;
+            }
+        }
+
+
         if (timerAnimation <= 0)
         {
             hasAnimationEnded = true;
+            startTimer2 = true;
+        }
+
+        if (timerAnimation2 <= 0)
+        {
+            hasAnimationEnded2 = true;
         }
 
         if(hasAnimationEnded == true)
         {
-            Inventory.instance.Remove(Inventory.instance.itemOnHand);
+            hacksawAnim.SetActive(false);
+            hacksawAnim2.SetActive(true);
+            sawAnimator2.enabled = true;
+        }
+
+        if(hasAnimationEnded2 == true)
+        {
             this.GetComponent<Rigidbody>().useGravity = true;
             this.GetComponent<Collider>().isTrigger = false;
             this.GetComponent<ItemPickup>().enabled = true;
-            hacksawAnim.SetActive(false);
             gasParticles.SetActive(true);
             gasStart = true;
         }
@@ -62,6 +91,7 @@ public class SawCollision : MonoBehaviour
                 GameObject.Destroy(child.gameObject);
             }
             Item.isInspecting = false;
+            Inventory.instance.Remove(Inventory.instance.itemOnHand);
             hacksawAnim.SetActive(true);
             sawAnimator.enabled = true;
             startTimer = true;
